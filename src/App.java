a86729e2522d8b4648473b39faf85fb1d5e8aaff
tkusal <thiago.kusal@outlook.com>
2022-09-14 02:23:26 -0300

    update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Mundo";
+        System.out.println("Olá, " + nome);
     }
 }
---
+++ src/mediaNota.java
@@ -0,0 +1,27 @@
+import javax.print.event.PrintEvent;
+import javax.swing.SingleSelectionModel;
+import java.util.Scanner;
+
+public class mediaNota {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        float nota1, nota2;
+        float media;
+        System.out.println("Digite a primeira nota: ");
+        nota1 = sc.nextFloat();
+
+        System.out.println("Digite a segunda nota: ");
+        nota2 = sc.nextFloat();
+
+        media = (nota1 + nota2)/2;
+        if (media >= 80){
+            System.out.println("Aprovado");
+        } else if (media < 80 && media >= 40) {
+            System.out.println("Aluno em recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+        System.out.println("A média é: " + media);
+    }
+}
